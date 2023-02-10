6fc16655d0135b0d1f4f325b556b7a6a1fc3903d
DragonSake <40611000+DragonSake@users.noreply.github.com>
2023-02-10 11:30:12 +0100

    Add files via upload

---
+++ visitekaartje/scripts/js.js
@@ -0,0 +1,20 @@
+const h1 = document.querySelector('h1');
+
+function fetchData (){
+    const url = 'https://whois.fdnd.nl/api/v1/member?id=cldstgnno3v9e0bw5sd7y75dp'
+
+    const data = fetch(url)
+                    .then(response => response.json())
+                    .then(data => {
+                        changeHTML(data)
+                    })
+    
+}
+
+function changeHTML (data) {
+    const name = data.member.name
+    const lastname = data.member.surname
+    h1.insertAdjacentHTML('beforeend', `${name} ${lastname}`)  
+}
+
+fetchData();

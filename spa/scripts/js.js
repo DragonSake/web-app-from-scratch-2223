ccff6f98cc575be17c5c514edcb7db7bc5186cc2
DragonSake <40611000+DragonSake@users.noreply.github.com>
2023-02-16 14:26:01 +0100

    Add files via upload

--- spa/scripts/js.js
+++ spa/scripts/js.js
@@ -1,23 +1,52 @@
+var data;
+var counter = 0;
 function fetchData (){
     const url = "https://type.fit/api/quotes"
 
-    const data = fetch(url)
+    fetch(url)
                     .then(response => response.json())
-                    .then(data => {
-                        changeH2(data)
+                    .then(val => {
+                        prevnext(val)
+                        data = val;
                         })
     }
+
+const h2 = document.querySelector('h2')
+const p = document.querySelector('p')
+
+const vorige = document.querySelector('button:first-of-type')
+const random = document.querySelector('li:nth-of-type(2) button')
+const next = document.querySelector('li:nth-of-type(3) button')
     
-    function changeH2(data) {
-        const quote = data[0].text
-        const naam = data[0].author
+function prevnext(data) {
+    const quote = data[counter].text
+    const naam = data[counter].author
 
-        const h2 = document.querySelector('h2')
-        const p = document.querySelector('p')
+    console.log(data)
 
-        console.log(data)
-        h2.insertAdjacentHTML('beforeend', `${quote} `)
-        p.insertAdjacentHTML('beforeend', `&mdash;  ${naam} `)
-    }
+    h2.textContent = `${quote} `;
+    p.textContent =  `~ ${naam} `;
+}
 
-    fetchData();
+fetchData();
+
+vorige.addEventListener("click", function() {
+    console.log(next);
+
+    counter--;
+    
+    prevnext(data);
+});
+
+next.addEventListener("click", function() {
+    console.log(next);
+    
+    counter++;
+    
+    prevnext(data);
+});
+
+random.addEventListener("click", function() {
+    console.log(random);
+
+});

0797ba44b6ef2c7e737d952d9881ecf79906a12a
DragonSake <40611000+DragonSake@users.noreply.github.com>
2024-02-07 14:18:36 +0100

    API from Justus isn't working anymore

--- visitekaartje/scripts/js.js
+++ visitekaartje/scripts/js.js
@@ -1,20 +1,22 @@
-const h1 = document.querySelector('h1');
+// API van Justus werkt niet meer
 
-function fetchData (){
-    const url = 'https://whois.fdnd.nl/api/v1/member?id=cldstgnno3v9e0bw5sd7y75dp'
+// const h1 = document.querySelector('h1');
 
-    const data = fetch(url)
-                    .then(response => response.json())
-                    .then(data => {
-                        changeHTML(data)
-                    })
-    
-}
+// function fetchData (){
+//     const url = 'https://whois.fdnd.nl/api/v1/member?id=cldstgnno3v9e0bw5sd7y75dp'
 
-function changeHTML (data) {
-    const name = data.member.name
-    const lastname = data.member.surname
-    h1.insertAdjacentHTML('beforeend', `${name} ${lastname}`)  
-}
+//     const data = fetch(url)
+//                     .then(response => response.json())
+//                     .then(data => {
+//                         changeHTML(data)
+//                     })
 
-fetchData();
+// }
+
+// function changeHTML (data) {
+//     const name = data.member.name
+//     const lastname = data.member.surname
+//     h1.insertAdjacentHTML('beforeend', `${name} ${lastname}`)  
+// }
+
+// fetchData();
